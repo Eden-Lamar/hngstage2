9b212f9239ac82d75d711e4aeb1cb37d54ba94a2
Eden Lamar <emmanuelmomodu3@gmail.com>
2021-08-21 20:32:20 +0100

    fix

--- mail.js
+++ mail.js
@@ -4,8 +4,8 @@ require("dotenv").config();
 
 const auth = {
 	auth: {
-		api_key: "1064ef90ba4d8f7e50eec15e385dac84-9776af14-aa718ee0",
-		domain: "sandbox114ed1ce456e4941bef7d120fc7b8536.mailgun.org",
+		api_key: process.env.API_KEY,
+		domain: process.env.DOMAIN,
 	},
 };
 
